76e86fc1405c26644d2b71f30521d94e4f4e7f93
JanefrancessC <janefrancesschioma@gmail.com>
2023-04-05 15:01:11 +0100

    creating routes

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -30,92 +30,100 @@ const CreatePost = () => {
   const generateImage = async () => {
     if (form.prompt) {
       try {
-        setGenerateImg(true)
-        const response = await fetch('http://localhost:5000/api/v1/dalle', {
-          method: 'POST',
-          headers: { 
-          'Content-Type': 'application/json',
-           },
-           body: JSON.stringify({ prompt: form.prompt }),
-        })
+        setGenerateImg(true);
+        const response = await fetch("http://localhost:5000/api/v1/dalle", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ prompt: form.prompt }),
+        });
+
+        const data = await response.json();
+
+        setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
       } catch (error) {
-        
+        alert(error);
+      } finally {
+        setGenerateImg(false);
       }
+    } else {
+      alert("Please enter a prompt");
     }
-  };
-
-  return (
-    <section className="max-w-7x1 mx-auto">
-      <div>
-        <h1 className="text-[32px] font-extrabold">Create</h1>
-        <p className="mt-2 text-[#666e75] text-[16px] max-w[500px]">
-          Create stunning AI images through DALL-E AI and share them with others
-        </p>
-      </div>
-
-      <form className="mt-16 max-w-3x1" onSubmit={handleSubmit}>
-        <div className="flex flex-col gap-5">
-          <FormField
-            labelName="Your name"
-            type="text"
-            name="name"
-            placeholder="Enter your name"
-            value={form.name}
-            handleChange={handleChange}
-          />
-          <FormField
-            labelName="Prompt"
-            type="text"
-            name="prompt"
-            placeholder="A comic book cover of a superhero wearing headphones"
-            value={form.prompt}
-            handleChange={handleChange}
-            isSurpriseMe
-            handleSurpriseMe={handleSurpriseMe}
-          />
-          <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
-            {form.photo ? (
-              <img
-                src={form.photo}
-                alt={form.prompt}
-                className="w-full h-full object-contain"
-              />
-            ) : (
-              <img
-                src={preview}
-                alt={preview}
-                className="w-9/12 h-9/12 object-contain opacity-40"
-              />
-            )}
-
-            {generateImg && (
-              <div className="absolute inset-0 z-0 flex justify-center items-centwe bg-[rgba(0,0,0,0,0.5)] rounder-lg">
-                <Loader />
-              </div>
-            )}
-          </div>
-        </div>
-        <div className="mt-5 flex gap-5">
-          <button
-            type="button"
-            onClick={generateImage}
-            className="text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
-          >
-            {generateImg ? "Generating..." : "Generate"}
-          </button>
-        </div>
-
-        <div className="mt-10">
-          <p className="mt-2 text-[#666e75] text-[14px]">
-            Share your AI image with the community
+  }
+    return (
+      <section className="max-w-7x1 mx-auto">
+        <div>
+          <h1 className="text-[32px] font-extrabold">Create</h1>
+          <p className="mt-2 text-[#666e75] text-[16px] max-w[500px]">
+            Create stunning AI images through DALL-E AI and share them with
+            others
           </p>
-          <button classname="mt-3 text-white bg-[#6469ff] " type="submit">
-            {loading ? "Sharing..." : "Share with community"}
-          </button>
         </div>
-      </form>
-    </section>
-  );
-};
+
+        <form className="mt-16 max-w-3x1" onSubmit={handleSubmit}>
+          <div className="flex flex-col gap-5">
+            <FormField
+              labelName="Your name"
+              type="text"
+              name="name"
+              placeholder="Enter your name"
+              value={form.name}
+              handleChange={handleChange}
+            />
+            <FormField
+              labelName="Prompt"
+              type="text"
+              name="prompt"
+              placeholder="A comic book cover of a superhero wearing headphones"
+              value={form.prompt}
+              handleChange={handleChange}
+              isSurpriseMe
+              handleSurpriseMe={handleSurpriseMe}
+            />
+            <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+              {form.photo ? (
+                <img
+                  src={form.photo}
+                  alt={form.prompt}
+                  className="w-full h-full object-contain"
+                />
+              ) : (
+                <img
+                  src={preview}
+                  alt={preview}
+                  className="w-9/12 h-9/12 object-contain opacity-40"
+                />
+              )}
+
+              {generateImg && (
+                <div className="absolute inset-0 z-0 flex justify-center items-centwe bg-[rgba(0,0,0,0,0.5)] rounder-lg">
+                  <Loader />
+                </div>
+              )}
+            </div>
+          </div>
+          <div className="mt-5 flex gap-5">
+            <button
+              type="button"
+              onClick={generateImage}
+              className="text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+            >
+              {generateImg ? "Generating..." : "Generate"}
+            </button>
+          </div>
+
+          <div className="mt-10">
+            <p className="mt-2 text-[#666e75] text-[14px]">
+              Share your AI image with the community
+            </p>
+            <button className="mt-3 text-white bg-[#6469ff] " type="submit">
+              {loading ? "Sharing..." : "Share with community"}
+            </button>
+          </div>
+        </form>
+      </section>
+    );
+  };
 
 export default CreatePost;
