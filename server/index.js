15c6ba98a37f56e090c5047f5305278e48b01879
JanefrancessC <janefrancesschioma@gmail.com>
2023-04-05 11:27:54 +0100

    creating routes

--- server/index.js
+++ server/index.js
@@ -2,12 +2,20 @@ import express from "express";
 import cors from "cors";
 import * as dotenv from "dotenv";
 import connectDB from "./mongodb/connect.js";
+import postRoutes from "./routes/postRoutes.js";
+import dalleRoutes from "./routes/dalleRoutes.js";
 
 dotenv.config();
+
 const PORT = process.env.PORT || 5000;
 const app = express();
+
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
+
+
 
 app.get("/", async (req, res) => {
   res.send("Welcome to AI image generator!");
@@ -18,7 +26,7 @@ const startServer = async (req, res) => {
     connectDB(process.env.MONGODB_URL);
     app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 };
 
